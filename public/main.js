ee442e13a40f43e248d05d639f50f2e27ee82820
Kenneth F Cannon <kennethfcannon@gmail.com>
2018-11-06 18:35:07 -0500

    Weather with Class!

--- public/main.js
+++ public/main.js
@@ -12,7 +12,7 @@
   newLI.textContent = `${weather.name}`
   // add the LI to the UL
   theUL.appendChild(newLI)
-}*/
+}
 
 const addMainTempWeatherToUL = weather => {
   // Find the UL
@@ -27,7 +27,7 @@ const addMainTempWeatherToUL = weather => {
   theUL.appendChild(newLI)
 }
 
-/*const addMainHumidityWeatherToUL = weather => {
+const addMainHumidityWeatherToUL = weather => {
   // Find the UL
   let theUL = document.querySelector('ul')
 
@@ -38,7 +38,7 @@ const addMainTempWeatherToUL = weather => {
   newLI.textContent = `${weather.main.humidity}`
   // add the LI to the UL
   theUL.appendChild(newLI)
-}*/
+}
 
 // Display data from site
 
@@ -57,7 +57,7 @@ const button = () => {
     .then(weatherData => {
       addCityNameWeatherToUL(weatherData)
     })
-}*/
+}
 
   fetch(inputUrl)
     .then(response => {
@@ -77,7 +77,105 @@ const button = () => {
     })
 }*/
 
+class WeatherAPI {
+  getWeatherForCity(city) {
+    let inputUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=cbb5b6537cd4a6eccd9aee04f4400e4e&units=imperial`
+
+    fetch(inputUrl)
+      .then(response => {
+        return response.json()
+      })
+      .then(weatherData => {
+        let temp = weatherData.main.temp
+        let humidity = weatherData.main.humidity
+
+        let windSpeed = weatherData.wind.speed
+
+        let dom = new DocumentObjectModel()
+        dom.substitute(temp, humidity, windSpeed)
+      })
+  }
+}
+
+class DocumentObjectModel {
+  substitute(temp, humidity, windSpeed) {
+    this.appendLI(temp)
+    this.appendLI(humidity)
+    this.appendLI(windSpeed)
+    // let newLI
+
+    // newLI = document.createElement('li')
+    // newLI.textContent = temp
+    // theUL.appendChild(newLI)
+
+    // newLI = document.createElement('li')
+    // newLI.textContent = humidity
+    // theUL.appendChild(newLI)
+  }
+
+  appendLI(value) {
+    let theUL = document.querySelector('ul')
+
+    let newLI = document.createElement('li')
+    newLI.textContent = value
+    theUL.appendChild(newLI)
+  }
+
+  city() {
+    let theInput = document.querySelector('.the-city-name').value
+    return theInput
+  }
+}
+
+handleUserClickedSearch = () => {
+  let dom = new DocumentObjectModel()
+  let city = dom.city()
+
+  let weatherAPI = new WeatherAPI()
+  weatherAPI.getWeatherForCity(city)
+}
+
 const main = () => {
-  document.querySelector('.search').addEventListener('click', button)
+  document
+    .querySelector('.search')
+    .addEventListener('click', handleUserClickedSearch)
 }
 document.addEventListener('DOMContentLoaded', main)
+
+/*class Data {
+  constructor() {
+    let inputUrl = `https://api.openweathermap.org/data/2.5/weather?q=${theInput}&APPID=cbb5b6537cd4a6eccd9aee04f4400e4e&units=imperial`
+
+    fetch(inputUrl)
+      .then(response => {
+        return response.json()
+      })
+      .then(weatherData => {
+        addToUL(weatherData)
+      })
+  }
+
+  city() {
+    let theInput = document.querySelector('.the-city-name').value
+  }
+
+  substitute() {
+    let theUL = document.querySelector('ul')
+
+    let newLI = document.createElement('li')
+    newLI.textContent = `${weather.main.temp}`
+
+    theUL.appendChild(newLI)
+
+    let newLI = document.createElement('li')
+    newLI.textContent = `${weather.main.sunrise}`
+
+    theUL.appendChild(newLI)
+
+    let newLI = document.createElement('li')
+    newLI.textContent = `${weather.main.sunset}`
+
+    theUL.appendChild(newLI)
+  }
+}
+*/
